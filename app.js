7e5cf891b6d3ba6f3f213efa9c0bfd4f496bd5fc
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2021-02-14 14:39:31 +0200

    Improve app structure

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./utils/config');
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+const app = express();
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
